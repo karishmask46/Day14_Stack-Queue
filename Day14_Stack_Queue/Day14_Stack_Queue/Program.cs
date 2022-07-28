a3cdc7d0ab83a4652e59ea642fd64cdd1f144e83
karishmask46 <karishmask.15999@gmail.com>
2022-07-28 19:34:49 +0530

    Creating Nodes

---
+++ Day14_Stack_Queue/Day14_Stack_Queue/Program.cs
@@ -0,0 +1,7 @@
+﻿using  Day14_Stack_Queue;
+Console.WriteLine("Welcome to the stack problem");
+CreatIngStack linkedListStack = new CreatIngStack();
+linkedListStack.Push(70);
+linkedListStack.Push(30);
+linkedListStack.Push(56);
+linkedListStack.Display();
