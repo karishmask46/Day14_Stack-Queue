54253b3c43a01500a0729a579826f566abeee005
karishmask46 <karishmask.15999@gmail.com>
2022-07-28 19:55:59 +0530

    creating Queue Data

---
+++ Day14_StackQueue/Program.cs
@@ -0,0 +1,7 @@
+﻿using Day14_StackQueue;
+Console.WriteLine("Welcome to the Queue problem");
+CreatingQueue linkedlistQueue = new CreatingQueue();
+linkedlistQueue.Enqueue(56);
+linkedlistQueue.Enqueue(30);
+linkedlistQueue.Enqueue(70);
+linkedlistQueue.Display();
